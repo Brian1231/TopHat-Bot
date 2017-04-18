494c115884265cd0627757fb4464e682fafa64ce
Brian O'Leary <brianoleary1995@gmail.com>
2017-04-18 13:16:45 +0100

    First

---
+++ MonopolyBot/src/TopHat.java
@@ -0,0 +1,35 @@
+public class TopHat implements Bot {
+	
+	// The public API of YourTeamName must not change
+	// You cannot change any other classes
+	// YourTeamName may not alter the state of the board or the player objects
+	// It may only inspect the state of the board and the player objects
+
+	
+	TopHat (BoardAPI board, PlayerAPI player, DiceAPI dice) {
+		this.board = board;
+		this.player = player;
+		this.dice = dice;
+	}
+	
+	private BoardAPI board;
+	private PlayerAPI player;
+	private DiceAPI dice;
+	
+	
+	public String getName () {
+		return "TopHat";
+	}
+	
+	public String getCommand () {
+		
+		// Add your code here
+		return "help";
+	}
+	
+	public String getDecision () {
+		// Add your code here
+		return "pay";
+	}
+	
+}
